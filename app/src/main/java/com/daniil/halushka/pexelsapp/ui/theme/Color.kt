08b7f206404ff47668f721b2179d46f40ea7ad2b
Daniil Halushka <popolzen22@gmail.com>
2024-02-03 23:29:18 +0300

    Initial commit. Add color palette

---
+++ app/src/main/java/com/daniil/halushka/pexelsapp/ui/theme/Color.kt
@@ -0,0 +1,15 @@
+package com.daniil.halushka.pexelsapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val redColor = Color(0xFFBB1020)
+val whiteColor = Color(0xFFFFFFFF)
+val blackColor = Color(0xFF1E1E1E)
+
+//lightColors
+val lightGreyColor = Color(0xFFF3F5F9)
+val lightDarkGreyColor = Color(0xFF868686)
+
+//nightColors
+val nightGreyColor = Color(0xFF393939)
+val nightDarkGreyColor = Color(0xFFB5B5B5)
